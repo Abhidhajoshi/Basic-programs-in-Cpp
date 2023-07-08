81bf045be9a69d993ca7cf236aa58954d1039271
Abhidha Joshi <124055802+Abhidhajoshi@users.noreply.github.com>
2023-07-08 21:38:45 +0530

    Add files via upload

---
+++ Add and subtract.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the first number ";
+    cin>>a;
+    cout<<"Enter the second number ";
+    cin>>b;
+    cout<<"Sum of "<<a<<" and "<<b<<" is "<<a+b<<endl;
+    cout<<"Difference of "<<a<<" and "<<b<<" is "<<a-b;
+    return 0;
+}
